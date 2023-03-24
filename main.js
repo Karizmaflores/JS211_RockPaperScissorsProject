8b66c3dc56f26536b9d6cd078a9d1ba24186d5b0
Karizmaflores <flores_karizma@yahoo.com>
2023-03-23 20:51:39 -0500

    completed first 2 checks

--- main.js
+++ main.js
@@ -13,7 +13,28 @@ const rl = readline.createInterface({
 
 // the function that will be called by the unit test below
 const rockPaperScissors = (hand1, hand2) => {
+  if(hand1 === hand2){
+    return "It's a tie!"
+  }
 
+  if (hand1 === 'rock' && hand2 === 'scissors') {
+    return "Hand one wins!"
+  }
+  else if (hand1 === 'rock' && hand2 === 'paper') {
+    return "Hand two wins!"
+  }
+   else if (hand1 === 'scissors' && hand2 === 'paper') {
+    return "Hand one wins!"
+  }
+   else if (hand1 === 'scissors' && hand2 === 'rock') {
+    return "Hand two wins!"
+  }
+   else if (hand1 === 'paper' && hand2 === 'rock') {
+    return "Hand one wins!"
+  }
+   else if (hand1 === 'paper' && hand2 === 'scissors') {
+    return "Hand two wins!"
+  }
   // Write code here
   // Use the unit test to see what is expected
 
